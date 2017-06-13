a1eedeb53c38b3214299359a4c8649eda466511c
sapdey <saptarshidey13@gmail.com>
2017-06-13 16:17:08 +0530

    Graph and functionality

--- js/main.js
+++ js/main.js
@@ -8,6 +8,7 @@
 
 			promise.then(function(response){
 				vm.stocks = response.data;
+				console.log(vm.stocks);
 
 			})
 			.catch(function(){
@@ -60,38 +61,108 @@
             vm.priceHeld = 0;
             
             vm.detail = function(){
-            	vm.net = []
+            	vm.net = [];
+            	vm.EPS = [];
+            	vm.finalGraph = [];
             	vm.netWorth = 0;
+            	vm.netEPS = 0;
             	for(var i = 0; i<vm.list().length; i++) {
             		vm.key = vm.list()[i].key;
             		vm.value = vm.list()[i].value;
             		vm.counter = vm.list()[i].counter;
 
-            		vm.priceHeld = vm.value * vm.counter;
-            		vm.net.push(vm.priceHeld);
-            	}
+            		vm.net.push(vm.value * vm.counter);
 
-            	//console.log(vm.key + ", " + vm.value + ", "+ vm.counter);
+            		vm.eps = vm.list()[i].eps;
+
+            		vm.EPS.push(vm.eps * vm.counter);
+
+            		vm.history = vm.list()[i].history.point;
+            		vm.graph = [];
+            		for (var j = 0; j<vm.history.length; j++) {
+            			vm.graph.push(vm.history[j].price * vm.counter);
+
+            		}
+            		
+            		vm.finalGraph.push(vm.graph);
+            	}
             	
             	for(var i in vm.net) {
             		vm.netWorth += vm.net[i];
             	}
-            }
+            	for (var i in vm.EPS) {
+            		vm.netEPS += vm.EPS[i];
+            	}
 
+            	vm.pe = vm.netWorth/vm.netEPS; // P/E Ratio
+
+            	for(var i=0; i<vm.finalGraph.length-1; i++) {
+
+            		vm.finalGraph[i+1] = vm.finalGraph[i].map(function (num, index) {
+            			return num + vm.finalGraph[i+1][index];
+            		});
+
+            	}
+            	vm.dataarr = vm.finalGraph[vm.finalGraph.length-1]
+            	console.log(vm.dataarr);
             	
-            
 
+            	Highcharts.chart('graph', {
+				    chart: {
+				        type: 'area'
+				    },
+				    title : "",
+				    xAxis: {
+				        allowDecimals: false,
+				        labels: {
+				            formatter: function () {
+				                return this.value;
+				            }
+				        }
+				    },
+				    yAxis: {
+				        title: {
+				            text: 'Value'
+				        },
+				        labels: {
+				            formatter: function () {
+				                return this.value;
+				            }
+				        }
+				    },
+				    tooltip: {
+				        pointFormat: '<b>{point.y:,.0f}</b>'
+				    },
+				    plotOptions: {
+				        area: {
+				            pointStart: 0,
+				            marker: {
+				                enabled: false,
+				                symbol: 'circle',
+				                radius: 2,
+				                states: {
+				                    hover: {
+				                        enabled: true
+				                    }
+				                }
+				            }
+				        }
+				    },
+				    series: [{
+				        name: 'Time',
+				        data: vm.dataarr
+				    }]
+				});
 
-
-           
+            } 
 	})
 
 	.factory('items', function(){
 		var items = [];
 		var itemsService = {};
     
-	    itemsService.add = function(key, value, counter) {
-	        items.push({key, value, counter});
+	    itemsService.add = function(key, value, counter, eps, history) {
+	        items.push({key, value, counter, eps, history});
 	    };
 	    itemsService.list = function() {
 	        return items;
